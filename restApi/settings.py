34b1ab052a4de9b769cf7adfc6a971526af8d221
muslim <muslim.krum.85@gmail.com>
2019-06-02 23:58:06 +0300

    add images

--- restApi/settings.py
+++ restApi/settings.py
@@ -128,7 +128,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/uploads/')
 
 AUTH_USER_MODEL = 'blog.User'
 
