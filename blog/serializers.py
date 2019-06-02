cb933c6c9f83b59ad6506a5fe6f7bd69edf8564d
muslim <muslim.krum.85@gmail.com>
2019-06-03 00:01:20 +0300

    Revert "Revert "add images""
    This reverts commit 9e3c41961e158cd13068872554baccc13fc69814.

--- blog/serializers.py
+++ blog/serializers.py
@@ -24,13 +24,31 @@ class CategorySerializers(serializers.ModelSerializer):
         fields = ("title", "image", "created_at")
 
 
+class PostFileSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = PostFile
+        fields = ("name", "path", "created_at")
+
+
 class PostSerializers(serializers.ModelSerializer):
     user = serializers.PrimaryKeyRelatedField(queryset=User.objects.all())  # UserSerializers(read_only=True)
     tags = serializers.PrimaryKeyRelatedField(many=True, queryset=Tag.objects.all())  # TagSerializers(many=True)
     category = serializers.PrimaryKeyRelatedField(
         queryset=Category.objects.all())  # CategorySerializers(read_only=True)
+    files = PostFileSerializers(many=True, read_only=True)
 
     class Meta:
         model = Post
-        fields = ("title", "slug", "body", "created_at", "user", "tags", "category")
+        fields = ("title", "slug", "body", "is_active", "created_at", "user", "tags", "category", "files")
+
+
+class PostReturnSerializer(serializers.ModelSerializer):
+    user = UserSerializers(read_only=True)
+    tags = TagSerializers(many=True)
+    category = CategorySerializers(read_only=True)
+    files = PostFileSerializers(many=True)
+
+    class Meta:
+        model = Post
+        fields = ("title", "slug", "body", "is_active", "created_at", "user", "tags", "category", "files")
 
--- blog/views/blog.py
+++ blog/views/blog.py
@@ -3,6 +3,15 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import permissions
 from rest_framework import parsers
+from django.core.files.storage import default_storage
+from django.core.exceptions import ValidationError
+import sys
+from PIL import Image
+from io import BytesIO
+from django.core.files.uploadedfile import InMemoryUploadedFile
+
+from django.conf import settings
+import os
 from django.shortcuts import get_object_or_404
 
 from blog.models import *
@@ -21,13 +30,27 @@ class Posts(APIView):
 class CreatePost(APIView):
     # IsAuthenticated
     permission_classes = (permissions.AllowAny,)
-    parser_classes = (parsers.JSONParser, parsers.MultiPartParser,)
+    parser_classes = (parsers.JSONParser, parsers.MultiPartParser, parsers.FileUploadParser,)
 
     def post(self, request):
         data = request.data
-
         serializer = PostSerializers(data=data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+            files = request.FILES.getlist('files')
+            post = Post.objects.latest('id')
+            valid_extensions = ['.png', '.jpg', '.gif']
+            for file in files:
+                filename = default_storage.generate_filename(settings.MEDIA_ROOT + '/' + file.name)
+                path = default_storage.save(filename, file)
+                ext = os.path.splitext(path)[1]
+                if not ext in valid_extensions:
+                    raise ValidationError(u'File not supported!', code='invalid')
+                try:
+                    PostFile.objects.create(name=file.name, path=path, post=post)
+                except ValueError:
+                    return Response({'error': 'Value Error'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+            serializer_post = PostReturnSerializer(instance=post)
+            return Response({'data': serializer_post.data}, status=status.HTTP_201_CREATED)
+
         return Response(serializer.errors, status=400)
