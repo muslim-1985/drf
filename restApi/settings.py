9ed5de6465607f924c9b8d121ff994963a490683
muslim <muslim.krum.85@gmail.com>
2019-06-03 00:02:13 +0300

    Revert "Revert "Revert "add images"""
    This reverts commit cb933c6c9f83b59ad6506a5fe6f7bd69edf8564d.

--- restApi/settings.py
+++ restApi/settings.py
@@ -128,7 +128,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/uploads/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 AUTH_USER_MODEL = 'blog.User'
 
